7f903b0e3860e7414109c19838f88f74a3aba5f9
PratikKondekar <pratik260397@gmail.com>
2025-06-26 21:17:36 +0530

    Add files via upload

---
+++ TEST 1.py
@@ -0,0 +1,10 @@
+num1=26
+num2=3
+addition=num1+num2
+subtraction=num1-num2
+multiplication=num1*num2
+division=num1/num2
+print("addition",num1+num2)
+print("subtraction",num1-num2)
+print("multiplication",num1*num2)
+print("division",num1/num2)
---
+++ Test 2.py
@@ -0,0 +1,4 @@
+first_name = "P"
+last_name = "K"
+full_name = first_name+last_name
+print("Hello" '\t'+full_name +"! welcome to the python program.")
